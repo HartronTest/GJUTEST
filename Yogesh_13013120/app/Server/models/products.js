3810c68b1450e1f8801ecff22fb538e80f23943b
HartronTest <parshanthartron@gmail.com>
2017-01-27 13:09:23 +0530

    Add files via upload

---
+++ Yogesh_13013120/app/Server/models/products.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+
+var schema = {
+	name: String,
+	price: Number,
+	category: String,
+	image: String,
+	brewery: String,
+	alcohol: Number	
+}
+
+var Products = mongoose.model("Products", schema);
+
+module.exports = Products;
